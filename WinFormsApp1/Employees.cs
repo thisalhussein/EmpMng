ce0ee413e0c38406053ffb36c795bffea3aa5552
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-28 00:58:21 +0200

    NOTE: VEEERRY IMPORTAANT COMMENT

--- WinFormsApp1/Employees.cs
+++ WinFormsApp1/Employees.cs
@@ -83,7 +83,7 @@ namespace WinFormsApp1
                 MessageBox.Show(Ex.Message);
             }
         }
-
+        
         private void DeleteBtn_Click(object sender, EventArgs e)
         {
             try
