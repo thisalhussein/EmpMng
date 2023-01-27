685134e28da46361f19fdc6e0b9eec905ce1f7a3
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 13:31:01 +0200

    Add table name

--- dbo.Table2.sql
+++ dbo.Table2.sql
@@ -1,5 +1,9 @@
 ﻿CREATE TABLE [dbo].[Table]
 (
 	[SCode] INT NOT NULL PRIMARY KEY, 
-    [Employee] INT NOT NULL
+    [Employee] INT NOT NULL, 
+    [Attendance] INT NOT NULL, 
+    [Period] VARCHAR(100) NOT NULL, 
+    [Amount] INT NOT NULL, 
+    [PayDate] DATE NOT NULL
 )
