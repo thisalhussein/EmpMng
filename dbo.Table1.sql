7374d8b8aa1e660d5c740c45d15a829ba4f59037
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 12:58:51 +0200

    Add Emp Date

--- dbo.Table1.sql
+++ dbo.Table1.sql
@@ -3,5 +3,6 @@
 	[EmpId] INT NOT NULL PRIMARY KEY, 
     [EmpName] VARCHAR(50) NOT NULL, 
     [EmpGen] VARCHAR(10) NOT NULL, 
-    [EmpDep] INT NOT NULL
+    [EmpDep] INT NOT NULL, 
+    [EmpDDB] DATE NOT NULL
 )
