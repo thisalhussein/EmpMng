970635cc518e1c34c1671aa2f2e1d64c08ee64f2
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 13:02:39 +0200

    Mkae EmpId Primary key

--- dbo.Table1.sql
+++ dbo.Table1.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[EmployeeTbl]
 (
-	[EmpId] INT NOT NULL PRIMARY KEY, 
+	[EmpId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [EmpName] VARCHAR(50) NOT NULL, 
     [EmpGen] VARCHAR(10) NOT NULL, 
     [EmpDep] INT NOT NULL, 
