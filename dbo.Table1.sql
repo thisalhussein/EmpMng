7a9746a8bfdb607e92ef8f4276d068915340af83
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 12:59:33 +0200

    Add employee date

--- dbo.Table1.sql
+++ dbo.Table1.sql
@@ -4,5 +4,6 @@
     [EmpName] VARCHAR(50) NOT NULL, 
     [EmpGen] VARCHAR(10) NOT NULL, 
     [EmpDep] INT NOT NULL, 
-    [EmpDDB] DATE NOT NULL
+    [EmpDDB] DATE NOT NULL, 
+    [EmpDate] DATE NOT NULL
 )
