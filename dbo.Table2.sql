3839a05014a38fe4a946624fb28d5be80de8304d
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 13:36:10 +0200

    Add Foriegn Key

--- dbo.Table2.sql
+++ dbo.Table2.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[SalaryTbl]
 (
-	[SCode] INT NOT NULL PRIMARY KEY, 
+	[SCode] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Employee] INT NOT NULL, 
     [Attendance] INT NOT NULL, 
     [Period] VARCHAR(100) NOT NULL, 
     [Amount] INT NOT NULL, 
-    [PayDate] DATE NOT NULL
+    [PayDate] DATE NOT NULL, 
+    CONSTRAINT [FK3] FOREIGN KEY ([Employee]) REFERENCES [EmployeeTbl]([EmpId])
 )
