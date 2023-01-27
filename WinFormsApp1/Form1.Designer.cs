cb8688a4ab8862f44bfb0fb4a0a9c7b594a2b58a
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 09:26:26 +0200

    change position

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -51,7 +51,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(234, 309);
+            this.textBox2.Location = new System.Drawing.Point(234, 290);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(291, 27);
             this.textBox2.TabIndex = 2;
