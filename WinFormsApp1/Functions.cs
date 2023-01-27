bb6f58aa6c9d2cb7ce6acd7f89f0036b779d9646
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 14:13:42 +0200

    Add library

--- WinFormsApp1/Functions.cs
+++ WinFormsApp1/Functions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,7 @@ namespace WinFormsApp1
             Cmd = new SqlCommand();
             Cmd.Connection = Con;
         }
+
+
     }
 }
