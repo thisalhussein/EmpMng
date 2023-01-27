9304dfcead20f18cf9e28e10c54b1b66db4eb428
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-27 14:05:33 +0200

    Edit in Function

---
+++ WinFormsApp1/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+        public Functions()
+        {
+
+        }
+    }
+}
