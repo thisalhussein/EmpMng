2b67d1cd71b1dcaff5e67a1bd8f7a716a6965865
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-28 00:22:15 +0200

    Fix error in table name proplem in line 136

--- WinFormsApp1/Employees.cs
+++ WinFormsApp1/Employees.cs
@@ -133,7 +133,7 @@ namespace WinFormsApp1
                     string DOB = DOBTb.Value.Date.ToString();
                     string JDate = JDateTb.Value.Date.ToString();
                     int Salary = Convert.ToInt32(DailySalTb.Text);
-                    string Query = "Update EmployeeTbl set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',EmpJDate='{4}',EmpSal={5} where EmpId= {6}";
+                    string Query = "Update EmployeeTbl set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDDB='{3}',EmpDate='{4}',EmpSal={5} where Empid= {6}";
                     Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
                     Con.SetData(Query);
                     ShowEmp();
@@ -148,6 +148,7 @@ namespace WinFormsApp1
             {
                 MessageBox.Show(Ex.Message);
             }
+            
         }
         int key =0;
         private void EmployeeList_CellContentClick(object sender, DataGridViewCellEventArgs e)
